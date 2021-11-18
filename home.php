77ecb23a13bce497fb5974918831f980eb339af6
rollly <marclunaris@gmail.com>
2021-11-18 21:20:59 +0300

    xnew

--- home.php
+++ home.php
@@ -1398,107 +1398,14 @@
                
                   <div
                       class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100"> 
-                              <button class="btn btn-primary" type="button">
+                              
+					<a class="btn btn-primary" href="portty.zip" download>
                                   <i class="fas fa-download"></i>	
-								  <span class="text">xDownload Portty</span>	
-                              </button>	
+								  <span class="text">Download Portty</span>					  
+					</a>	  
+							  
                   </div>
-
-<?php 
-
-// Create zip
-function createZip($zip,$dir){
-    if (is_dir($dir)){
-
-        if ($dh = opendir($dir)){
-            while (($file = readdir($dh)) !== false){
-                
-                // If file
-                if (is_file($dir.$file)) {
-                    if($file != '' && $file != '.' && $file != '..'){
-                        
-                        $zip->addFile($dir.$file);
-                    }
-                }else{
-                    // If directory
-                    if(is_dir($dir.$file) ){
-
-                        if($file != '' && $file != '.' && $file != '..'){
-
-                            // Add empty directory
-                            $zip->addEmptyDir($dir.$file);
-
-                            $folder = $dir.$file.'/';
-                            
-                            // Read data of the folder
-                            createZip($zip,$folder);
-                        }
-                    }
-                    
-                }
-                    
-            }
-            closedir($dh);
-        }
-    }
-}
-    
-
-	
-// Download Created Zip file
-if(isset($_POST['downloadporttyzip'])){
-	
-	
-	$zip = new ZipArchive();
-    $filename = "./portty.zip";
-
-    if ($zip->open($filename, ZipArchive::CREATE)!==TRUE) {
-        exit("cannot open <$filename>\n");
-    }
-
-    $dir = 'exe/';
-    //$dir = './portty-dashboard/exe/';
-
-    // Create zip
-    createZip($zip,$dir);
-
-    $zip->close();	
-	
-    
-    $filename = "portty.zip";
-
-    if (file_exists($filename)) {
-        header('Content-Type: application/zip');
-        header('Content-Disposition: attachment; filename="'.basename($filename).'"');
-        header('Content-Length: ' . filesize($filename));
-
-        flush();
-        readfile($filename);
-        // delete file
-        //unlink($filename);
-    
-
-    }
-}	
-
-?>				  
-
-                  <form method='post' action=''
-                      class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                      <div class="input-group">                          
-                          <div class="input-group">	
-                              <button class="btn btn-primary" type="button">
-                                  <i class="fas fa-download"></i>	
-								  <span class="text">Download Portty</span>	
-                              </button>	
-                          </div>												
-						<div class="input-group"> 
-						<input class="btn btn-primary" type='submit' name='downloadporttyzip' value='Download Portty' />				  
-						</div>
-						  
-                      </div>
-                  </form>
-				  
+		
 				  
                <!-- Topbar Navbar -->
                <ul class="navbar-nav ml-auto">
